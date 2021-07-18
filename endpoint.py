d0d04911865a3c14d9bd087fb4973ec386a76334
Patrick <65928079+HippoBaguette@users.noreply.github.com>
2021-07-18 14:09:43 +0800

    /image/slap endpoint done

--- endpoint.py
+++ endpoint.py
@@ -8,10 +8,24 @@ app = flask.Flask(__name__)
 def home():
     return "Nothing seems to be here", 404
 
-@app.route('/', methods=['POST'])
-def create():
+@app.route('/image', methods=['POST'])
+def image():
     requestbody = request.get_json(force=True)
-    return requestbody, 404
+    return "404, not found", 404
+
+@app.route('/image/slap', methods=['POST'])
+def image_slap():
+    requestbody = request.get_json(force=True)
+
+    avatarURL = "avatarURL" in requestbody
+    
+    if avatarURL:
+        if (type(requestbody["avatarURL"]) is list):
+            if len(requestbody["avatarURL"]) == 2:
+                if r"https://cdn.discordapp.com/avatars/" in requestbody["avatarURL"][0] and r"https://cdn.discordapp.com/avatars/" in requestbody["avatarURL"][1]:
+                        return "200, OK", 200
+    
+    return "400, bad request", 400
 
 @app.errorhandler(400)
 def bad_request(e):
