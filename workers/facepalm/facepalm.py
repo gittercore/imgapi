74c37c0b479eaa77a8ebbe5b89a71645477e8bae
Patrick <65928079+HippoBaguette@users.noreply.github.com>
2021-07-21 17:46:19 +0800

    Encode to base 64, store ids

--- workers/facepalm/facepalm.py
+++ workers/facepalm/facepalm.py
@@ -1,7 +1,9 @@
 import requests
+import base64
+import os
 from PIL import Image
 
-def facepalm(target: str, caller: str) -> str:
+def facepalm(target: str, caller: str, id: int) -> bytes:
     caller_avatar_raw = Image.open(requests.get(caller, stream=True).raw)
     target_avatar_raw = Image.open(requests.get(target, stream=True).raw)
     
@@ -12,4 +14,9 @@ def facepalm(target: str, caller: str) -> str:
     
     result = template.copy()
     result.paste(caller_avatar, (152, 60))
-    result.show()
+    result.save("out/facepalm.png")
+    
+    encoded = base64.b64encode(open("out/facepalm.png", "rb").read()).decode()
+    if os.path.exists("out/facepalm.png"):
+         os.remove("demofile.txt")
+    return 'data:image/png;base64,{}'.format(encoded)
